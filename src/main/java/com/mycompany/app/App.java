a56293622df7563bbcc5bfbcef711c6a1dcdb5a3
HpsMac <56122618+HpsMac@users.noreply.github.com>
2019-11-22 17:26:52 -0600

    Editando para disparar vesp1

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! " );
+        System.out.println( "Hello World! Hubo un cambio y se disparo la tarea" );
     }
 }
