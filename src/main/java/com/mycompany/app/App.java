240c03d28822634fda80e5ccd54b148629832279
HpsMac <56122618+HpsMac@users.noreply.github.com>
2019-11-22 19:49:31 -0600

    commit para disparar vesp2

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Hubo un cambio y se disparo la tarea" );
+        System.out.println( "Hello World! Hubo un cambio mas y se disparo la tarea" );
     }
 }
